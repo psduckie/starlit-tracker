545107bb864317e5694bab7a25a24ca78dfb0e22
PSDuckie <psduckie@yahoo.com>
2019-02-27 06:31:48 -0700

    Ran the linter on index.js.
    Still need to test the bot.

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var output = "";
 var disc;
 const filled = "█";
 const empty = "░";
+var healthString;
+var initNumber;
+var initString;
+var progress;
 
 // Create the chatbot interface
 const client = new Discord.Client();
@@ -22,17 +26,19 @@ const dbConnection = MySQL.createConnection({
 
 // Log a ready message when ready
 client.on("ready", () => {
+	// eslint-disable-next-line no-console
 	console.log("Connected to the chat server.");
 });
 
 // Connect to the database
 dbConnection.connect(function (err) {
 	if (err) { throw err; }
+	// eslint-disable-next-line no-console
 	console.log("Connected to the database.");
 });
 
 // Read from database
-function parseSignups(value, index, array) {
+function parseSignups(value/*, index, array*/) {
 	if (output.length > 1500) {
 		disc.channel.send(output);
 		output = "";
@@ -44,33 +50,33 @@ function parseSignups(value, index, array) {
 		output += `${value.player} signed up as ${value.chara} (Team ${value.team})\n`;
 	}
 }
-function parseSignupsDM(value, index, array) {
+function parseSignupsDM(value/*, index, array*/) {
 	if (output.length > 1500) {
 		disc.author.send(output);
 		output = "";
 	}
 	output += `ID: ${value.id}, Player: ${value.player}, Character: ${value.chara}, Team: ${value.team}\n`;
 }
-function track(value, index, array) {
-	console.log(value);
+function track(value/*, index, array*/) {
+//	console.log(value);
 	if (output.length > 1500) {
 		disc.author.send(output);
 		output = "";
 	}
 	if (value.enabled === 0) {
-		output += `??? :triangular_flag_on_post:\n`;
+		output += "??? :triangular_flag_on_post:\n";
 	}
 	else {
 		let numChars = Math.round(((value.progress / value.maxProgress) * 100) / 10);
-		console.log(`NumChars: ${numChars}`);
+		//console.log(`NumChars: ${numChars}`);
 		output += `${value.description} `;
 		for (let i = 0; i < numChars; i++) {
 			output += filled;
-			console.log(`${i}: Filled`);
+			//console.log(`${i}: Filled`);
 		}
 		for (let i = numChars; i < 10; i++) {
 			output += empty;
-			console.log(`${i}: Empty`);
+			//console.log(`${i}: Empty`);
 		}
 		if (value.progress < value.maxProgress) {
 			output += " :triangular_flag_on_post:\n";
@@ -80,14 +86,14 @@ function track(value, index, array) {
 		}
 	}
 }
-function trackDM(value, index, array) {
+function trackDM(value/*, index, array*/) {
 	if (output.length > 1500) {
 		disc.author.send(output);
 		output = "";
 	}
 	output += `ID: ${value.id}, Description: ${value.description}, Progress: ${value.progress}, Max Progress: ${value.maxProgress}, Enabled: ${value.enabled}\n`;
 }
-function parseHealth(value, index, array) {
+function parseHealth(value/*, index, array*/) {
 	var healthEntry = "`";
 	healthEntry += value.charAbbrev.toUpperCase();
 	for (var i = healthEntry.length; i <= 5; i++) {
@@ -108,27 +114,27 @@ function parseHealth(value, index, array) {
 	}
 	healthString += (healthEntry + "\n");
 }
-function parseHealthDM(value, index, array) {
+function parseHealthDM(value/*, index, array*/) {
 	if (output.length > 1500) {
 		disc.author.send(output);
 		output = "";
 	}
 	output += `ID: ${value.id}, Char Name: ${value.charName}, Char Abbrev: ${value.charAbbrev.toUpperCase()}, Health: ${value.health}, Health Icon: ${value.healthIcon.toLowerCase()}, Killable: ${value.killable}\n`;
 }
-function parseInit(value, index, array) {
+function parseInit(value/*, index, array*/) {
 	var initEntry = `${initNumber}: ${value.charName} (${value.init}) {ID: ${value.id}}\n`;
 	initNumber++;
 	initString += initEntry;
 }
 
 // Write to database
-function updateProgress(value, index, array) {
-	this.progress = value.progress + 1;
-	dbConnection.query(`UPDATE tracker SET progress = ${this.progress} WHERE id = ${value.id};`);
+function updateProgress(value/*, index, array*/) {
+	progress = value.progress + 1;
+	dbConnection.query(`UPDATE tracker SET progress = ${progress} WHERE id = ${value.id};`);
 }
-function updateHealth(value, index, array, amount) {
+function updateHealth(value/*, index, array*/, amount) {
 	this.health = value.health + amount;
-	dbConnection.query(`UPDATE health SET health = ${this.health} WHERE id = ${value.id};`)
+	dbConnection.query(`UPDATE health SET health = ${this.health} WHERE id = ${value.id};`);
 }
 function doDamage(value, index, array) {
 	updateHealth(value, index, array, -1);
@@ -142,23 +148,26 @@ client.login(config.token);
 
 // Listen for a message
 client.on("message", message => {
+	var args;
+	var param;
+
 	if (message.content === `${config.prefix}test`) { // Test message
-		console.log(message.author);
+		//console.log(message.author);
 		//		dbConnection.query(`INSERT INTO test(test) VALUES('${message.author.username}');`);
-		message.reply(`test received.`);
+		message.reply("test received.");
 	}
 	if (message.content.startsWith(`${config.prefix}signup `)) { // Insert into database
-		var param = message.content.slice(8);
+		param = message.content.slice(8);
 		param = param.replace(/'/g, "''");
-		console.log(param);
+		//console.log(param);
 		dbConnection.query(`INSERT INTO signups(player, chara) VALUES('${message.author.username}', '${param}');`);
 		message.member.addRole(message.guild.roles.find("name", "Storyline Players")); // Mark the user as having inserted a record into the database
-		message.reply(`signup received.`);
+		message.reply("signup received.");
 	}
 	if (message.content === `${config.prefix}signuplist`) { // Read from database
-		dbConnection.query("SELECT player, chara, team FROM signups;", function (err, result, fields) {
+		dbConnection.query("SELECT player, chara, team FROM signups;", function (err, result/*, fields*/) {
 			if (err) { throw err; }
-			console.log(result);
+			//console.log(result);
 			output = "";
 			disc = message;
 			result.forEach(parseSignups);
@@ -167,8 +176,8 @@ client.on("message", message => {
 	}
 	if (message.content === `${config.prefix}signuplist dm`) { // Table dump from database	
 		if (message.member.roles.exists("name", "Storyline DM")) { // Authenticate user; if it succeeds, dump the table into a private message
-			console.log("If statement hit");
-			dbConnection.query("SELECT * FROM signups;", function (err, result, fields) {
+			//console.log("If statement hit");
+			dbConnection.query("SELECT * FROM signups;", function (err, result/*, fields*/) {
 				output = "";
 				disc = message;
 				result.forEach(parseSignupsDM);
@@ -182,7 +191,7 @@ client.on("message", message => {
 		}
 	}
 	if (message.content === `${config.prefix}track`) {
-		dbConnection.query("SELECT description, progress, maxProgress, enabled FROM tracker;", function (err, result, fields) {
+		dbConnection.query("SELECT description, progress, maxProgress, enabled FROM tracker;", function (err, result/*, fields*/) {
 			if (err) { throw err; }
 			output = `**${config.title}**\n`;
 			disc = message;
@@ -192,7 +201,7 @@ client.on("message", message => {
 	}
 	if (message.content === `${config.prefix}track dm`) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			dbConnection.query("SELECT * FROM tracker;", function (err, result, fields) {
+			dbConnection.query("SELECT * FROM tracker;", function (err, result/*, fields*/) {
 				output = "";
 				disc = message;
 				result.forEach(trackDM);
@@ -207,13 +216,12 @@ client.on("message", message => {
 	}
 	if (message.content.startsWith(`${config.prefix}progress `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var param = message.content.slice(10);
-			var progress;
-			console.log(`Param: ${param}`);
-			dbConnection.query(`SELECT id, progress FROM tracker WHERE id = ${param};`, function (err, result, fields) {
+			param = message.content.slice(10);
+			//console.log(`Param: ${param}`);
+			dbConnection.query(`SELECT id, progress FROM tracker WHERE id = ${param};`, function (err, result) {
 				result.forEach(updateProgress);
 			});
-			message.reply(`update received.`);
+			message.reply("update received.");
 		}
 		else // Authentication failed; print access denied message
 		{
@@ -222,18 +230,19 @@ client.on("message", message => {
 	}
 	if (message.content.startsWith(`${config.prefix}enable `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var param = message.content.slice(8);
+			param = message.content.slice(8);
 			dbConnection.query(`UPDATE tracker SET enabled = 1 WHERE id = ${param};`);
-			message.reply(`update received.`);
+			message.reply("update received.");
 		}
 		else {
 			message.reply("access denied.");
 		}
 	}
 	if (message.content === `${config.prefix}health`) {
-		dbConnection.query("SELECT charAbbrev, health, healthIcon, killable FROM health;", function (err, result, fields) {
+		dbConnection.query("SELECT charAbbrev, health, healthIcon, killable FROM health;", function (err, result) {
 			healthString = "";
 			result.forEach(parseHealth);
+			// eslint-disable-next-line no-console
 			console.log(healthString);
 			if (healthString != "") {
 				message.channel.send(healthString);
@@ -245,7 +254,7 @@ client.on("message", message => {
 	}
 	if (message.content === `${config.prefix}health dm`) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			dbConnection.query("SELECT * FROM health;", function (err, result, fields) {
+			dbConnection.query("SELECT * FROM health;", function (err, result) {
 				output = "";
 				disc = message;
 				result.forEach(parseHealthDM);
@@ -265,101 +274,108 @@ client.on("message", message => {
 	}
 	if (message.content.startsWith(`${config.prefix}damage `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var param = message.content.slice(8);
-			dbConnection.query(`SELECT id, health FROM health WHERE id = ${param};`, function (err, result, fields) {
+			param = message.content.slice(8);
+			dbConnection.query(`SELECT id, health FROM health WHERE id = ${param};`, function (err, result) {
 				result.forEach(doDamage);
 			});
-			message.reply(`damage applied.`);
+			message.reply("damage applied.");
 		}
 		else {
-			message.reply(`access denied.`);
+			message.reply("access denied.");
 		}
 	}
 	if (message.content.startsWith(`${config.prefix}heal `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var param = message.content.slice(6);
-			dbConnection.query(`SELECT id, health FROM health WHERE id = ${param};`, function (err, result, fields) {
+			param = message.content.slice(6);
+			dbConnection.query(`SELECT id, health FROM health WHERE id = ${param};`, function (err, result) {
 				result.forEach(doHealing);
 			});
-			message.reply(`healing applied.`);
+			message.reply("healing applied.");
 		}
 		else {
-			message.reply(`access denied.`);
+			message.reply("access denied.");
 		}
 	}
 	if (message.content.startsWith(`${config.prefix}init add `)) {
-		var args = message.content.split(' ');
+		args = message.content.split(" ");
 		try {
-			let name = args.slice(2, args.length - 1).join(' ');
+			let name = args.slice(2, args.length - 1).join(" ");
 			dbConnection.query(`INSERT INTO initiative(charName, init) VALUES("${name}", ${args[args.length - 1]});`);
 			message.channel.send("Added " + name + " to the initiative order.");
 		} catch (e) {
+			// eslint-disable-next-line no-console
 			console.log(e);
 			message.author.send(e); //This needs to be changed eventually
 		}
 	}
 	if (message.content.startsWith(`${config.prefix}init remove `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var args = message.content.split(' ');
+			args = message.content.split(" ");
 			try {
 				dbConnection.query(`DELETE FROM initiative WHERE id=${args[2]};`);
 				message.channel.send("Unit removed.");
 			} catch (e) {
+				// eslint-disable-next-line no-console
 				console.log(e);
 				message.author.send(e);
 			}
 		}
 		else {
-			message.reply(`access denied.`);
+			message.reply("access denied.");
 		}
 	}
 	if (message.content.startsWith(`${config.prefix}init switch `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var args = message.content.split(' ');
+			args = message.content.split(" ");
 			try {
+				// eslint-disable-next-line no-console
 				console.log(args);
 				dbConnection.query(`UPDATE initiative AS first JOIN initiative AS second SET first.init = second.init, second.init = first.init WHERE first.id = ${args[2]} AND second.id = ${args[3]};`);
 				message.channel.send("Unit order switched.");
 			} catch (e) {
+				// eslint-disable-next-line no-console
 				console.log(e);
 				message.author.send(e);
 			}
 		}
 		else {
-			message.reply(`access denied.`);
+			message.reply("access denied.");
 		}
 	}
 	if (message.content.startsWith(`${config.prefix}init name `)) {
 		if (message.member.roles.exists("name", "Storyline DM")) {
-			var args = message.content.split(' ');
+			args = message.content.split(" ");
 			try {
-				dbConnection.query(`UPDATE initiative SET charName="${args.slice(3).join(' ')}" WHERE id=${args[2]};`);
+				dbConnection.query(`UPDATE initiative SET charName="${args.slice(3).join(" ")}" WHERE id=${args[2]};`);
 				message.channel.send("Unit renamed.");
 			} catch (e) {
+				// eslint-disable-next-line no-console
 				console.log(e);
 				message.author.send(e);
 			}
 		}
 		else {
-			message.reply(`access denied.`);
+			message.reply("access denied.");
 		}
 	}
 	if (message.content === `${config.prefix}init order`) {
 		try {
-			dbConnection.query("SELECT id, charName, init FROM initiative ORDER BY init DESC;", function(err, result, fields) {
+			dbConnection.query("SELECT id, charName, init FROM initiative ORDER BY init DESC;", function(err, result) {
 				initString = "";
 				initNumber = 1;
 				result.forEach(parseInit);
+				// eslint-disable-next-line no-console
 				console.log(initString);
 				if(initString != "") {
-					message.channel.send("**Initiative Order**")
+					message.channel.send("**Initiative Order**");
 					message.channel.send(initString);
 				}
 				else {
 					message.reply("I can't show the initiative records because there are no initiative records to show.");
 				}
-			})
+			});
 		} catch (e) {
+			// eslint-disable-next-line no-console
 			console.log(e);
 			message.author.send(e);
 		}
@@ -369,7 +385,7 @@ client.on("message", message => {
 			dbConnection.query("TRUNCATE TABLE initiative;");
 		}
 		else {
-			message.reply(`access denied.`);
+			message.reply("access denied.");
 		}
 	}
 });
